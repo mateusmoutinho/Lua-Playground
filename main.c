ab1040f167cd5d9ecc8c6da16ab00c343320cd59
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-05-03 01:55:16 -0300

    att

--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 c2wasm_js_var execute(){
     LuaCEmbed *lua_virtual_machine = newLuaCEmbedEvaluation();
-    
-    
+
     c2wasm_js_var id_collect_args  = c2wasm_create_array();
     c2wasm_append_array_string(id_collect_args, "codeEditor");
     c2wasm_js_var code = c2wasm_call_object_prop(c2wasm_document,"getElementById", id_collect_args);
+
+    string_si
     return c2wasm_undefined;
 }
 
